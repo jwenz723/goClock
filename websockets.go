f8afa44eb24dd183a22137ccee7374eb992f6c4e
jeff.wenzbauer <jwenz723@gmail.com>
2018-04-09 11:15:14 -0600

    added websockets demo

---
+++ websockets.go
@@ -0,0 +1,68 @@
+// websockets.go
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"time"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			alarmtime := ""
+			if string(msg) != "start" {
+				alarmtime = string(msg)
+			}
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+
+			go func(a string) {
+				for {
+					t := time.Now().Local()
+					s := t.Format("03:04")
+
+					if s == a {
+						// Write message back to browser
+						if err = conn.WriteMessage(websocket.TextMessage, []byte("alarm!")); err != nil {
+							return
+						}
+					} else {
+						// Write message back to browser
+						if err = conn.WriteMessage(websocket.TextMessage, []byte(s)); err != nil {
+							return
+						}
+					}
+
+					time.Sleep(15 * time.Second)
+				}
+			}(alarmtime)
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
